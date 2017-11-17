4b69ec002322bc7105aa7e1f270df2906940fe38
cannabistchronicles <email@example.com>
2017-11-16 22:08:31 -0500

    sdsd

--- server.js
+++ server.js
@@ -11,12 +11,11 @@ app.use('/', blog_route);
 
 // parse application/json 
 function init() {
-    mongoose.connect('mongodb://localhost:27017/blog'); 
+    mongoose.connect('mongodb://104.154.207.204:27017/blog'); 
     //mongoose.connect('mongodb://slabby:36lx4mDRu2dko6es@cluster0-shard-00-00-xbnzf.mongodb.net:27017,cluster0-shard-00-01-xbnzf.mongodb.net:27017,cluster0-shard-00-02-xbnzf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',{useMongoClient: true});
      var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
      db.once('open', function() {
-         console.log("A");
 
     });
 }
